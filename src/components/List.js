332119eebb7b21106f177708a96454d6083b8f35
a1000rhan <1000rhan@gmail.com>
2022-01-23 13:52:40 +0300

    nav

--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,11 @@
 import React from "react";
 import dataStore from "../store/dataStore";
-import Listitem from "./Listitem"
+import Listitem from "./Listitem";
 
 const List = () => {
-<<<<<<< HEAD
-  const showList = dataStore.tasks.map ((list) => <Listitem list = {list} />);
+  const showList = dataStore.tasks.map((list) => <Listitem list={list} />);
 
-  return <div> 
-    {showList}
-  </div>;
-=======
-  return (
-    <div>
-      <h1>List</h1>
-    </div>
-  );
->>>>>>> 653e45d64cb4f254fb5e9a63423bdfea6baaa099
+  return <div>{showList}</div>;
 };
 
 export default List;
