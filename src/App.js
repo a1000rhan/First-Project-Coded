764838978d1f3a3de23028eb5660e40c5f3348d2
a1000rhan <1000rhan@gmail.com>
2022-01-23 11:47:41 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import List from "./components/List";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/List" element={<List />} />
+        <Route path="/list" element={<List />} />
       </Routes>
     </div>
   );
