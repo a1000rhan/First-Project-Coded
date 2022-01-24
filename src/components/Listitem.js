d5ce17c413be4f36edbd51467858a2b38b429bb9
a1000rhan <1000rhan@gmail.com>
2022-01-24 11:19:30 +0300

    style list done

--- src/components/Listitem.js
+++ src/components/Listitem.js
@@ -1,29 +1,17 @@
-import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
 
-function Listitem({list}) {
+function Listitem({ list }) {
   return (
-      // <div>
-      // <div className='container-list'>
-      // <div className='align-list'>
-      // <div className='text-list'><h1 className='h1-list'>{list.title}</h1></div>
-      // <img className='image-list' src= {list.image} />
-      // </div>
-      // </div>
-      // </div>
-     <div>
-      <Card className='card-list'>
-        <div className='image-div'><Card.Img variant="top" className='image-list' src={list.image} /></div>
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+    <div className="conatiner-list">
+      <div className="card-list">
+        <div className="image-list">
+          <img className="theimage" src={list.image} />
+        </div>
+        <p className="title-list">{list.title}</p>
       </div>
-  ) 
+    </div>
+  );
 }
 
 export default Listitem;
