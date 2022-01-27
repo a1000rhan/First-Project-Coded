88ab626db1b48f6bde66a822cc80e3d2add4d74f
a1000rhan <1000rhan@gmail.com>
2022-01-27 13:20:43 +0300

    slideshow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
+import SimpleImageSlider from "react-simple-image-slider";
+import image1 from "../image1.jpg";
+import image2 from "../image2.jpg";
+import image3 from "../image3.jpg";
 
 const Home = () => {
+  const images = [
+    {
+      url: image1,
+    },
+    {
+      url: image2,
+    },
+    {
+      url: image3,
+    },
+  ];
   return (
     <div>
       <h1 className="title-home">Ga6eaTna</h1>
-      <Link to="/aboutus">
-        <div className="container-home">
-          <img
+      <div className="container-home">
+        <SimpleImageSlider
+          width={800}
+          height={504}
+          images={images}
+          showBullets={true}
+          showNavs={true}
+          navStyle={2}
+          autoPlay={true}
+        />
+        {/* <img
             src="https://images-ext-2.discordapp.net/external/mZpLjEp6LxdXGzJCFpbhc36bVCB6ZxnTL85H9S4rkmA/https/www.clipartkey.com/mpngs/m/4-40753_cartoon-money-clipart-money-bag-clipart-png.png"
             className="home-img"
             alt="homePage"
-          />
-        </div>
-      </Link>
+          /> */}
+      </div>
       <Footer />
       {/* <Routes>
         <Route path="/" element={<Home />} />
